73341bb79a458c6e05ec2405b29d8d6eafd06c45
Leticia <leticiacoutolx@gmail.com>
2024-03-05 18:40:59 -0300

    Add new screen class 3xl -> 1920px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,14 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    }
   },
   plugins: [],
 };

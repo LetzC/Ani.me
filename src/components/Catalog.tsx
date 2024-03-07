d6aabf07e5e73b61815cd0e7c82b9f1e63014f9e
Leticia <leticiacoutolx@gmail.com>
2024-03-07 16:50:07 -0300

    Catalog anime filtered without news and refactoring

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -9,13 +9,13 @@ const Catalog = ({ getNew = false }: { getNew?: boolean }) => {
   const isSmall = useMediaQuery({ query: "(max-width: 768px)" });
   const [visibleAnimes, setVisibleAnimes] = useState(isSmall ? 4 : 12);
 
-  const filteredAnimesDatabse = database.filter((anime) =>
-    getNew ? anime.isNew : !anime.isNew
+  const filteredAnimesDatabase = database.filter((anime) =>
+    getNew ? anime.isNew : !anime.isNew && anime.type !== "news"
   );
 
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-x-4 gap-y-11 md:gap-y-14">
-      {filteredAnimesDatabse.slice(0, visibleAnimes).map((anime) => (
+      {filteredAnimesDatabase.slice(0, visibleAnimes).map((anime) => (
         <div
           key={anime.id}
           className="col-span-1 w-full max-w-48 hover:scale-110 transition-transform cursor-pointer mx-auto"
@@ -23,10 +23,10 @@ const Catalog = ({ getNew = false }: { getNew?: boolean }) => {
           <CardAnime anime={anime} />
         </div>
       ))}
-      {filteredAnimesDatabse.length > 4 &&
-        visibleAnimes < filteredAnimesDatabse.length && (
+      {filteredAnimesDatabase.length > 4 &&
+        visibleAnimes < filteredAnimesDatabase.length && (
           <button
-            onClick={() => setVisibleAnimes(filteredAnimesDatabse.length)}
+            onClick={() => setVisibleAnimes(filteredAnimesDatabase.length)}
             className="bg-gray-900 py-2 w-48 col-span-full mx-auto mb-8 rounded"
           >
             Ver Mais

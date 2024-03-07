7dfdbe6ce9e95710f304c4567ca22f7abecac9da
Leticia <leticiacoutolx@gmail.com>
2024-03-07 17:24:27 -0300

    Carousel responsive text and margin

--- src/components/CarouselAnimes.tsx
+++ src/components/CarouselAnimes.tsx
@@ -17,11 +17,12 @@ const CarouselAnimes = () => {
       interval={5000}
       showArrows={false}
       showStatus={false}
+      className="my-10"
     >
       {filteredNewsDatabase.map((animeNew, index) => (
         <div
           key={`slide${index + 1}`}
-          className="flex flex-col gap-y-2 xl:gap-y-4 place-content-end min-h-[180px] md:h-80 2xl:h-96 px-4 py-10 md:px-7 2xl:px-8"
+          className="flex flex-col gap-y-2 xl:gap-y-4 place-content-end min-h-[180px] sm:h-80 2xl:h-96 px-4 py-10 md:px-7 2xl:px-8"
         >
           {animeNew.urlCover && (
             <>
@@ -42,10 +43,10 @@ const CarouselAnimes = () => {
                 }`}
                 className="rounded-lg"
               />
-              <p className="relative max-w-[65%] uppercase text-white font-medium text-sm md:text-2xl 3xl:text-4xl text-left z-10">
+              <p className="relative md:max-w-[65%] uppercase text-white font-medium text-sm md:text-2xl 3xl:text-4xl text-left z-10">
                 {animeNew.title}
               </p>
-              <p className="relative max-w-[50%] text-gray-300 text-xs md:text-sm 2xl:text-lg text-left z-10">
+              <p className="relative md:max-w-[50%] text-gray-300 text-xs md:text-sm 2xl:text-lg text-left z-10">
                 {animeNew.description}
               </p>
             </>

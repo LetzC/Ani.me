d35bc2e1f9d899f198e17f1b9fda67c0ed73a3a7
Leticia <leticiacoutolx@gmail.com>
2024-03-07 12:17:09 -0300

    Add new anime session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import Catalog from "@/components/Catalog";
 import Image from "next/image";
 
+const titleStyle = "font-medium text-lg mb-8 md:text-2xl 2xl:text-3xl md:mb-10";
+
 export default function Home() {
   return (
     <main>
       <section>
-        <h3
-          className={`font-medium text-lg mb-8 
-        md:text-2xl 2xl:text-3xl md:mb-10`}
-        >
-          Últimas atualizações
-        </h3>
+        <h3 className={titleStyle}>Últimas atualizações</h3>
         <Catalog />
+      </section>
+
+      <div className="h-px w-full bg-[#151B26] my-12 xl:my-16"></div>
+
+      <section>
+        <h3 className={titleStyle}>Animes recentes</h3>
+        <Catalog getNew={true} />
       </section>
     </main>
   );

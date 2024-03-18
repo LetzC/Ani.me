90487da03609dbeff236fd6604b9b306bf221c43
Leticia <leticiacoutolx@gmail.com>
2024-03-18 15:16:03 -0300

    Button full width via props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,20 @@ import React from "react";
 type ButtonProps = {
   children: string;
   onClickFunction: () => void;
+  fullWidth?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, onClickFunction }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClickFunction,
+  fullWidth,
+}) => {
+  const buttonClasses = `bg-gray-900 py-2 ${
+    fullWidth ? "w-full" : "w-48"
+  } col-span-full mx-auto rounded`;
+
   return (
-    <button
-      onClick={onClickFunction}
-      className="bg-gray-900 py-2 w-48 col-span-full mx-auto rounded"
-    >
+    <button onClick={onClickFunction} className={buttonClasses}>
       {children}
     </button>
   );

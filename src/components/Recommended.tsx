f7b76ea7c66ada579a8b39ae4a83169364f1e261
Leticia <leticiacoutolx@gmail.com>
2024-03-28 12:13:46 -0300

    Add getLowAnimeTitle to Recommended

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,7 +1,8 @@
+import animeService from "@/app/services/animeService";
 import databaseAnime from "../../database-animes.json";
 import Link from "next/link";
 
-const Recommended = ({ animeId }: { animeId: Number }) => {
+const Recommended = ({ animeId }: { animeId: number }) => {
   // Removendo o animeId do database e embaralhando o database
   const filteredDatabase = databaseAnime
     .filter((anime) => anime.id !== animeId)
@@ -10,28 +11,36 @@ const Recommended = ({ animeId }: { animeId: Number }) => {
   const randomAnimes = filteredDatabase.slice(0, 4);
 
   if (randomAnimes.length > 0) {
-    return randomAnimes.map((anime) => (
-      <Link
-        href={`/animes/${anime.id}/${anime.title
-          .replace(/\s+/g, "-")
-          .toLowerCase()}`}
-        key={anime.id}
-        className="bg-[#151B26] flex items-center h-full"
-      >
-        <div
-          className="min-w-28 h-full"
-          style={{
-            backgroundImage: `url(${anime.urlImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        ></div>
-        <div className="p-4">
-          <h5 className="font-medium text-lg">{anime.title}</h5>
-          <p className="mt-3 text-[#BFBFBF]">{anime.releaseDate}</p>
-        </div>
-      </Link>
-    ));
+    return randomAnimes.map((anime) => {
+      let animeTitle: string;
+      try {
+        animeTitle = animeService.getLowAnimeTitle(anime.id);
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+
+      return (
+        <Link
+          href={`/animes/${anime.id}/${animeTitle}`}
+          key={anime.id}
+          className="bg-[#151B26] flex items-center h-full"
+        >
+          <div
+            className="min-w-28 h-full"
+            style={{
+              backgroundImage: `url(${anime.urlImage})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+          <div className="p-4">
+            <h5 className="font-medium text-lg">{anime.title}</h5>
+            <p className="mt-3 text-[#BFBFBF]">{anime.releaseDate}</p>
+          </div>
+        </Link>
+      );
+    });
   } else {
     return (
       <h1 className="text-center text-[#BFBFBF]">Nenhum anime recomendado</h1>

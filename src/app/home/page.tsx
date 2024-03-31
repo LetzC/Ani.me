40cba61fc326b92bd2f9baf11ddc9f4220b08fe5
Leticia <leticiacoutolx@gmail.com>
2024-03-31 19:48:25 -0300

    Removal of the home page, as it was unused

--- src/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = {
-  title: "Home | Ani.me",
-  description: "Página com o catálogo dos animes.",
-};
-const Home = () => {
-  return <h1>Home</h1>;
-};
-
-export default Home;
